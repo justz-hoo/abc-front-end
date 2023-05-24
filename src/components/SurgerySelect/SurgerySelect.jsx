b024fdf508d595e16bf0ad88197a4923dfd8d8da
justz-hoo <1067744962@qq.com>
2023-05-24 15:55:07 +0800

    update

---
+++ src/components/SurgerySelect/SurgerySelect.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import Select from 'react-select';
+import {forwardRef, useImperativeHandle} from "react";
+
+const SurgerySelect = (props) => {
+    useImperativeHandle(props.cRef, () => {
+        return {
+            getType: selectedOption
+        }
+    })
+
+    const options = [
+        { value: '1', label: '一类切口手术' },
+        { value: '2', label: '二类切口手术' },
+        { value: '3', label: '血管类手术项目' },
+    ];
+    const [selectedOption, setSelectedOption] = useState(options[0]);
+
+    return (
+        <div className='select'>
+            <div className="surgery-options">
+                <Select
+                    className="react-select-container"
+                    defaultValue={selectedOption}
+                    onChange={(e) => setSelectedOption(e)}
+                    options={options}
+                    classNamePrefix='surgery-select'
+                />
+            </div>
+        </div>
+    );
+}
+export default forwardRef(SurgerySelect);
---
+++ src/components/SurgeryTime/SurgeryTime.jsx
@@ -0,0 +1,234 @@
+import React, {useEffect, useImperativeHandle, useState} from "react";
+import {styled} from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import InputBase from "@mui/material/InputBase";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import { forwardRef  } from 'react';
+import Paper from '@mui/material/Paper';
+
+
+const SurgeryTime = (props) => {
+
+    useImperativeHandle(props.cRef, () => {
+        return {
+            // setData,
+            getData: data  //暴露给父组件的接口
+        }
+    })
+
+    const [data, setData] = useState(null);
+    const [options, setOption] = useState(null);
+    useEffect(() => {
+        setData([
+            {name: '', usage: ''},
+            // {name: '', usage: ''},
+            // {name: '', usage: ''},
+        ]);
+
+        setOption([
+            {value: '丙泊酚', label: '丙泊酚'},
+            {value: '罗库酰胺', label: '罗库酰胺'},
+            {value: '芬太尼', label: '芬太尼'},
+            {value: '七氟醚', label: '七氟醚'},
+            {value: '氧气', label: '氧气'},
+            {value: '硫酸镁', label: '硫酸镁'}
+        ]);
+    }, []);
+
+
+
+    const handleAdd = (e) => {
+        const newData = {name: null, usage: null};
+        setData((prev) => [
+            ...prev,
+            newData
+        ])
+    };
+
+    const handleDel = (e, index) => {
+        setData((prev) => {
+            const newData = [];
+            for (let i = 0; i < prev.length; i++) {
+                if (i === index) continue;
+                newData.push(prev[i]);
+            }
+            return newData;
+        });
+    };
+
+    const handleChange = (e, index) => {
+        setData((prev) => {
+            const newData = [];
+            const tmp = prev[index];
+            tmp.name = e.target.value;
+            for (let i = 0; i < prev.length; i++) {
+                if (index === i) {
+                    newData.push(tmp);
+                    continue;
+                }
+                newData.push(prev[i]);
+            }
+            return newData;
+        });
+    }
+
+    const handleChangeNum = (e, index) => {
+        // console.log(e.target.value);
+        setData((prev) => {
+            const newData = [];
+            const tmp = prev[index];
+            tmp.usage = e.target.value;
+            for (let i = 0; i < prev.length; i++) {
+                if (index === i) {
+                    newData.push(tmp);
+                    continue;
+                }
+                newData.push(prev[i]);
+            }
+            return newData;
+        });
+    }
+
+    const CssTextField = styled(TextField)({
+        '& label.Mui-focused': {
+            color: '#CBDCF7',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: '#CBDCF7',
+
+        },
+        '& .MuiOutlinedInput-root': {
+            borderRadius: 6,
+            '& fieldset': {
+                borderColor: '#CBDCF7',
+            },
+            '&:hover fieldset': {
+                borderColor: '#2684FF',
+                boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#2684FF',
+                borderWidth: 1.5,
+            },
+        },
+    });
+
+    const CssInput = styled(InputBase)({
+        '& label.Mui-focused': {
+            backgroundColor: '#CBDCF7',
+        },
+        '& .MuiInputBase-input': {
+            borderColor: '#CBDCF7',
+            padding: '8.5px 20px 8.5px 12px',
+            border: '1px solid #CBDCF7',
+            borderRadius: 6,
+            '&:hover': {
+                borderColor: '#2684FF'
+            },
+            '&:focused': {
+                borderColor: '#2684FF'
+            },
+        },
+    })
+
+    return (
+        <div className='surgery-block'>
+            <div style={{fontStyle: 16, fontWeight: 600}}>{props.time}</div>
+            <TableContainer component={Paper} style={{padding: 5}}>
+                <Table  size="small" aria-label="a dense table" style={{border: "none",}}>
+                    <TableHead style={{border:"none"}}>
+                        <TableRow>
+                            <TableCell style={{borderBottom: 0}} align="left">材料名称</TableCell>
+                            <TableCell style={{borderBottom: 0}} align="left">使用数量</TableCell>
+                            <TableCell style={{borderBottom: 0}} align='left'>编辑</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody style={{border:"none"}}>
+                        {data && data.map((record, index) => (
+                            <TableRow key={index}>
+                                <TableCell align="left" style={{borderBottom: 0}} key={index}>
+                                    <div className="drug-options">
+                                        <FormControl sx={{minWidth: 120 }} style={{width: 300, backgroundColor: '#F7FAFF', borderColor: '#2684FF'}} size="small">
+                                            <InputLabel id="demo-select-small-label">name</InputLabel>
+                                            <Select
+                                                required
+                                                labelId="demo-select-small-label"
+                                                id="demo-select-small"
+                                                label="name"
+                                                value={record.name}
+                                                input={<CssInput/>}
+                                                onChange={(e) => handleChange(e, index)}
+                                            >
+                                                {options.map((option, index) =>
+                                                    <MenuItem value={option.value}>{option.value}</MenuItem>
+                                                )}
+                                            </Select>
+                                        </FormControl>
+                                    </div>
+                                </TableCell>
+                                <TableCell style={{borderBottom: 0}}>
+                                    <CssTextField
+                                        required
+                                        label="amount"
+                                        name="usage"
+                                        id="custom-css-outlined-input"
+                                        size={"small"}
+                                        value={record.usage}
+                                        onChange={(e) => handleChangeNum(e, index)}
+                                        style={{width: 300, backgroundColor: '#F7FAFF'}}
+                                    />
+                                </TableCell>
+                                {index === data.length - 1 ? (
+                                    <TableCell align='left' style={{borderBottom: 0}}>
+                                        {data.length > 1 && (
+                                            <Button
+                                                variant="outlined"
+                                                style={{marginRight: 20}}
+                                                onClick={(e) => handleDel(e, index)}>Del</Button>
+                                        )}
+                                        {data.length === 1 ? (
+                                            <>
+                                                <Button
+                                                    variant="outlined"
+                                                    style={{marginRight: 20}}
+                                                    onClick={(e) => handleAdd(e)}>Add</Button>
+                                                <Button
+                                                    style={{marginRight: 20}}
+                                                    disabled
+                                                ></Button>
+                                            </>
+                                        ):(
+                                            <Button
+                                                variant="outlined"
+                                                style={{marginRight: 20}}
+                                                onClick={(e) => handleAdd(e)}>Add</Button>
+                                        )}
+                                    </TableCell>
+                                ): (
+                                    <TableCell align='left' style={{borderBottom: 0}}>
+                                        <Button
+                                            variant="outlined"
+                                            style={{marginRight: 20}}
+                                            onClick={(e) => handleDel(e, index)}>Del</Button>
+                                    </TableCell>
+                                )}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
+}
+
+export default forwardRef(SurgeryTime);
